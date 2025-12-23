90ca433a73f14295a515ea604ff100b2539ea2fa
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-12-23 19:09:52 +0530

    Registered apps to settings

--- projecttracker/settings.py
+++ projecttracker/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Applications
+    'accounts',
+    'projects',
+    'tasks',
 ]
 
 MIDDLEWARE = [
