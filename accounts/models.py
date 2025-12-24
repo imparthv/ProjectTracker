0432705eed14f4516d92950ae32ae4cc883e8d41
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-12-24 18:23:52 +0530

    Updated last_login field to correctly show last login time

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
 
 class Account(AbstractUser):
     # Extending djangos build in User using AbstractUser
     email = models.CharField(max_length=100, unique=True, blank=False)
+    last_login = models.DateTimeField(default=now)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
